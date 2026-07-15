0279ea93ad3676cb67e0892c033d99441e69a7fb
AGOSSA Finagnon Thierry Belmar <belmaragossa@gmail.com>
2026-07-15 16:32:50 +0100

    Implement database connection and test endpoint
    Added environment variable support for database connection and a new endpoint to test the database connection.

--- app/main.py
+++ app/main.py
@@ -1,7 +1,15 @@
+import os
 from fastapi import FastAPI
+import psycopg2
 
 
 app = FastAPI()
+
+DB_HOST = os.getenv("DB_HOST", "")
+DB_PORT = os.getenv("DB_PORT", "")
+DB_NAME = os.getenv("DB_NAME", "")
+DB_USER = os.getenv("DB_USER", "")
+DB_PASSWORD = os.getenv("DB_PASSWORD", "")
 
 
 @app.get("/")
@@ -12,3 +20,33 @@ def home():
 @app.get("/ping")
 def ping():
     return {"status": "success", "message": "Ping reussi"}
+
+
+@app.get("/db-test")
+def test_db_connection():
+    try:
+        # Tentative de connexion au serveur Postgres mutualisé
+        connection = psycopg2.connect(
+            host=DB_HOST,
+            port=DB_PORT,
+            database=DB_NAME,
+            user=DB_USER,
+            password=DB_PASSWORD,
+            connect_timeout=3
+        )
+        cursor = connection.cursor()
+        cursor.execute("SELECT version();")
+        db_version = cursor.fetchone()
+        cursor.close()
+        connection.close()
+        
+        return {
+            "status": "success",
+            "message": "Connexion à PostgreSQL réussie !",
+            "postgres_version": db_version[0]
+        }
+    except Exception as e:
+        return {
+            "status": "error",
+            "message": f"Échec de la connexion : {str(e)}"
+        }
